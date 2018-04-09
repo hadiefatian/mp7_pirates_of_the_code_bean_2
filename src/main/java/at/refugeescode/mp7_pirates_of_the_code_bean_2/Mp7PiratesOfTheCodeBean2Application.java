ecb234e53cb0bb392827cc7f8b921f35a43368c9
Hadi <hadi.effatian@gmail.com>
2018-04-09 12:16:10 +0200

    Created Project

---
+++ src/main/java/at/refugeescode/mp7_pirates_of_the_code_bean_2/Mp7PiratesOfTheCodeBean2Application.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp7_pirates_of_the_code_bean_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp7PiratesOfTheCodeBean2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp7PiratesOfTheCodeBean2Application.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp7_pirates_of_the_code_bean_2/Mp7PiratesOfTheCodeBean2ApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp7_pirates_of_the_code_bean_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp7PiratesOfTheCodeBean2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
